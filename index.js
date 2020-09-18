5b8e583e128137a5037fe02a360a05c36edb20a5
Nick <nickmarron@hotmail.co.uk>
2020-09-18 10:08:32 +0100

    .

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.use(express.json());
 const usersRouter = require("./routes/users");
 app.use("/", usersRouter);
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log(`Listening on ${PORT}`);
 });
