18526a3dcfef3bd92530e1486a5ca3e4239c7ef3
Nick <nickmarron@hotmail.co.uk>
2020-09-18 13:01:27 +0100

    tidy up

--- database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-const connection = "mongodb+srv://admin:marvelouscranes@cranecluster.6taew.mongodb.net/craneCluster?retryWrites=true&w=majority";
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log("Database Connected Successfully"))
-    .catch(err => console.log(err));
